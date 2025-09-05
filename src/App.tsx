6d656b76db8d1c74196d6d404f06abc0a48b3302
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-05 23:08:34 +0300

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,32 @@ import { useState } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import type { Todo } from "./types";
+import { v4 as uuidv4 } from "uuid";
 
 import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleAddTodo = (title: string) => {
+  const handleAddTodo = (todo: {
+    title: string;
+    dueDate: string;
+    priority: string;
+  }) => {
     const newTodo: Todo = {
-      id: Date.now(),
-      title,
+      id: uuidv4(),
+      title: todo.title,
+      dueDate: todo.dueDate,
+      priority: todo.priority,
     };
+
     console.log("New todo: ", newTodo);
     setTodos([...todos, newTodo]);
   };
 
-  const handleDeleteTodo = (id: number) => {
-    setTodos(todos.filter((todos) => todos.id !== id));
+  const handleDeleteTodo = (id: string) => {
+    const filteredTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
   };
 
   return (
