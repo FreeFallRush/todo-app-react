714e83614ed82e2a61a67a00d34a178f8f6e2f64
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-05 22:15:34 +0300

    Updated TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,10 +3,13 @@ import TodoItem from "./TodoItem";
 
 interface TodoListProps {
   todos: Todo[];
-  onDelete: (id: number) => void;
+  onDelete: (id: string) => void;
 }
 
 function TodoList({ todos, onDelete }: TodoListProps) {
+  if (todos.length === 0) {
+    return <p>No todos yet!</p>;
+  }
   return (
     <ul>
       {todos.map((todo) => (
