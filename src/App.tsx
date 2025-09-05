0074d2f47b5523022d1d5b5e7e13bde4e812437a
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-05 19:12:50 +0300

    Added TodoForm component in TodoForm.tsx
    Imported Todo interface type and TodoForm component in App.tsx for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import type { Todo } from "./types";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleaAddTodo = (title: string) => {
+    const newTodo: Todo = {
+      id: todos.length + 1,
+      title,
+    };
+    console.log("New todo: ", newTodo);
+    setTodos([...todos, newTodo]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TodoForm onAdd={handleaAddTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+interface TodoFormProps {
+  onAdd: (title: string) => void;
+}
+
+function TodoForm({ onAdd }: TodoFormProps) {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+    onAdd(title);
+    setTitle("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Enter a new todo..."
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+export default TodoForm;
