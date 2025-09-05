dd52652e2d157336d97c4a98fbd40483327a03fe
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-05 21:16:11 +0300

    Added dueDateand priority to Todo interface
    Added type interface for Project

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,14 @@
 export interface Todo {
   id: number;
   title: string;
+  dueDate?: string;
+  priority?: string;
+}
+
+export interface Project {
+  id: number;
+  name: string;
+  description?: string;
+  color?: string;
+  todos: Todo[];
 }
