9321dcc5d34026794de0cee6006864e79533fbfe
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-05 19:48:52 +0300

    Added TodoItem component in TodoItem.tsx

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,3 +1,17 @@
-function TodoItem() {}
+import type { Todo } from "../types";
+
+interface TodoItemProps {
+  todo: Todo;
+  onDelete: (id: number) => void;
+}
+
+function TodoItem({ todo, onDelete }: TodoItemProps) {
+  return (
+    <li>
+      {todo.title}
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </li>
+  );
+}
 
 export default TodoItem;
