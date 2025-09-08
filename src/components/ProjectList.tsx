8095f001269bd24f037ce5a69c73d7a0dd02415f
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-08 12:23:31 +0300

    Added ProjectList.tsx to components with ProjectList component

---
+++ src/components/ProjectList.tsx
@@ -0,0 +1,37 @@
+import type { Project } from "../types";
+import ProjectItem from "./ProjectItem";
+
+interface ProjectListProps {
+  projects: Project[];
+  onDeleteProject: (id: string) => void;
+  onAddTodo: (
+    projectId: string,
+    todo: { title: string; dueDate: string; priority: string }
+  ) => void;
+  onDeleteTodo: (projectId: string, todoId: string) => void;
+}
+
+function ProjectList({
+  projects,
+  onDeleteProject,
+  onAddTodo,
+  onDeleteTodo,
+}: ProjectListProps) {
+  if (projects.length === 0) return <p>No projects yet1</p>;
+
+  return (
+    <div>
+      {projects.map((project) => (
+        <ProjectItem
+          key={project.id}
+          project={project}
+          onDelete={onDeleteProject}
+          onAddTodo={onAddTodo}
+          onDeleteTodo={onDeleteTodo}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ProjectList;
