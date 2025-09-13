d441f406630f46bbdbf4b0fd8f45ccfa40c8b803
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-09-13 22:32:58 +0300

    Fixed imported styles in ImportantTodosPage.tsx
    Added styles for ImportantTodosPage component in AllPages.css

--- src/components/pages/ImportantTodosPage.tsx
+++ src/components/pages/ImportantTodosPage.tsx
@@ -3,8 +3,7 @@ import type { Project, Todo } from "../../types";
 import TodoList from "../TodoList";
 import TodoForm from "../TodoForm";
 import Modal from "../Modal";
-import "../../styles/AllProjectsPage.css";
-
+import "../../styles/AllPages.css";
 interface ImportantTodosPageProps {
   projects: Project[];
   onDeleteTodo: (projectId: string, todoId: string) => void;
